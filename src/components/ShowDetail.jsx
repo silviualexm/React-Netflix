3f5353a4857bb0028ed2b6a3b8ceca09689ea70d
Silviu Alexandru Murgu <silviu.murgu@yahoo.com>
2021-03-31 17:57:23 +0300

    at deadline

---
+++ src/components/ShowDetail.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class ShowDetail extends React.Component {
+    
+
+    componentDidMount = () => {
+        let id = this.props.match.params.id
+        let selected = 
+    }
+
+    render(){
+        return(
+            <>
+
+            </>
+        )
+    }
+}
+
+export default ShowDetail
