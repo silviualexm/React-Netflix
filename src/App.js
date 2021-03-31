2ab28d625d9db9f022d585d0f8dc80d49da343cc
Silviu Alexandru Murgu <silviu.murgu@yahoo.com>
2021-03-31 18:02:03 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './components/NavBar'
 import "./components/NavBar";
+import { BrowserRoute as Router, Route } from 'react-router-dom'
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import NetflixNavBar from './components/NavBar'
@@ -11,9 +12,13 @@ function App() {
   return (
     
       <div>
-        <NetflixNavBar />
-        <Home />
-        <Footer />
+        <Router>
+          <NetflixNavBar />
+          <Home />
+          <Route path="/ShowDetail/:id" component="ShowDetail" />
+          <Footer />
+        </Router>
+        
       </div>
 
   )
